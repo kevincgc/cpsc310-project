f77ba8fbe40bf076514c66af24114e1dd3b71e04
Kevin <kevincgc@gmail.com>
2021-10-25 16:53:29 -0700

    updated validateQuery for c2, added tests, all grammar tests pass

---
+++ src/controller/ValidateQuery Helpers.ts
@@ -0,0 +1,96 @@
+import {InsightDatasetKind} from "./IInsightFacade";
+
+// From https://stackoverflow.com/a/6283527
+export function count(obj: any) {
+	return Object.keys(obj).length;
+}
+
+export function isString(input: any): boolean {
+	return (typeof input === "string" || input instanceof String);
+}
+
+export function isArray(input: any): boolean {
+	return (input instanceof Array);
+}
+
+export function isObject(input: any): boolean {
+	return (input instanceof Object);
+}
+
+export function isValidKey (input: string, id: string, kind: InsightDatasetKind) {
+	return (isValidRoomKey(input, id) && kind === InsightDatasetKind.Rooms) ||
+		(isValidCourseKey(input, id) && kind === InsightDatasetKind.Courses);
+}
+
+export function isValidMKey (input: string, id: string, kind: InsightDatasetKind) {
+	return (isValidRoomMKey(input, id) && kind === InsightDatasetKind.Rooms) ||
+		(isValidCourseMKey(input, id) && kind === InsightDatasetKind.Courses);
+}
+
+export function isValidSKey (input: string, id: string, kind: InsightDatasetKind) {
+	return (isValidRoomSKey(input, id) && kind === InsightDatasetKind.Rooms) ||
+		(isValidCourseSKey(input, id) && kind === InsightDatasetKind.Courses);
+}
+
+export function isValidCourseKey (input: string, id: string) {
+	return isValidCourseMKey(input, id) || isValidCourseSKey(input, id);
+}
+
+export function isValidRoomKey (input: string, id: string) {
+	return isValidRoomMKey(input, id) || isValidRoomSKey(input, id);
+}
+
+export function isValidCourseMKey (input: string, id: string) {
+	const validMKeys = [/^[^_]+_avg$/, /[^_]+_pass$/, /[^_]+_fail$/, /[^_]+_audit$/, /[^_]+_year$/];
+	return validMKeys.some((rx) => rx.test(input)) && input.split("_")[0] === id;
+}
+
+export function isValidCourseSKey (input: string, id: string) {
+	const validSKeys = [/^[^_]+_dept$/, /[^_]+_id$/, /[^_]+_instructor$/, /[^_]+_title$/, /[^_]+_uuid$/];
+	return validSKeys.some((rx) => rx.test(input)) && input.split("_")[0] === id;
+}
+
+export function isValidRoomMKey (input: string, id: string) {
+	const validMKeys = [/^[^_]+_lat$/, /[^_]+_lon$/, /[^_]+_seats$/];
+	return validMKeys.some((rx) => rx.test(input)) && input.split("_")[0] === id;
+}
+
+export function isValidRoomSKey (input: string, id: string) {
+	const validSKeys = [/^[^_]+_fullname$/, /[^_]+_shortname$/, /[^_]+_number$/, /[^_]+_name$/,
+		/[^_]+_address$/, /[^_]+_type$/, /[^_]+_furniture$/, /[^_]+_href$/];
+	return validSKeys.some((rx) => rx.test(input)) && input.split("_")[0] === id;
+}
+
+export function getDatasetInfo(query: any) {
+	let response = { id: "", kind: InsightDatasetKind.Rooms, err: false };
+	if (query["TRANSFORMATIONS"]) {
+		let id = query["TRANSFORMATIONS"]["GROUP"][0].split("_")[0];
+		response.id = id;
+		if(isValidRoomKey(query["TRANSFORMATIONS"]["GROUP"][0], id)) {
+			response.kind = InsightDatasetKind.Rooms;
+		} else if (isValidCourseKey(query["TRANSFORMATIONS"]["GROUP"][0], id)) {
+			response.kind = InsightDatasetKind.Courses;
+		} else {
+			response.err = true;
+		}
+	} else {
+		let id = query["OPTIONS"]["COLUMNS"][0].split("_")[0];
+		response.id = id;
+		if(isValidRoomKey(query["OPTIONS"]["COLUMNS"][0], id)) {
+			response.kind = InsightDatasetKind.Rooms;
+		} else if (isValidCourseKey(query["OPTIONS"]["COLUMNS"][0], id)) {
+			response.kind = InsightDatasetKind.Courses;
+		} else {
+			response.err = true;
+		}
+	}
+	return response;
+}
+
+export function getArrayKeys(input: any): any[] {
+	let keys = [];
+	for (let key of input) {
+		keys.push(key);
+	}
+	return keys;
+}
